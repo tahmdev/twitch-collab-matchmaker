024fd2ae60735ff318a63c1db3a6a114c70f6020
tahmdev <qwertiimusic@gmail.com>
2022-05-15 19:40:12 +0200

    Added profile functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ import { Routes, Route, Link } from "react-router-dom";
 import Navbar from './components/navbar';
 import Profile from './routes/profile';
 
+// Profile
+// => gender should be radio buttons
+// => age should be number input with min & max
+// => tags, up to 10
+// Looking for
+// => Create profile of perfect partner
+// => Age should be a range
+// => Gender should be checkboxes
+// Settings
+// => Lightmode 
+// => Delte account
+// Match
+// => Generate queue of matches
+//  => Age & gender should be exact matches, tags should be the more the better + allow mandatory tags
+// => On decline
+//  => add to declined list 
+// => On accept 
+//  => add to accepted list
+//    => Check if partners accepted list includes user 
+//      => If true: add both to websocket room + send notification 
+//      => else: check if user is on partners declined list
+//        => if false: add  user to partners match queue
+
 function App() {
   let [auth, setAuth] = useState(() => {
     let cookie = Cookies.get("twitchMatchMakerauthCookie")
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,21 +1,36 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import BirthdayInput from "../components/birthdayInput"
+import Select from 'react-select'
+import moment from "moment"
 
 const Profile = ({auth}) => {
   let [edit, setEdit] = useState(false)
   let [description, setDescription] = useState("loading...")
-  let [age, setAge] = useState("loading...")
+  let [birthday, setBirthday] = useState("1900-01-01")
+
   let [gender, setGender] = useState("loading...")
+  let prevRef = useRef()
+ 
   useEffect(() => {
     fetch(`http://localhost:9000/db/getUser/${auth.sessionID}`)
     .then(res => res.json())
     .then(json => {
       setDescription(json[0].description || "")
-      setAge(json[0].age || "")
+      setBirthday(json[0].birthday || "")
       setGender(json[0].gender || "")
     })
   }, [])
   
-  const save = () => {
+  const handleEdit = () => {
+    prevRef.current = {
+      description: description,
+      birthday: birthday,
+      gender: gender,
+    }
+    setEdit(true)
+  }
+
+  const handleSave = () => {
     fetch(`http://localhost:9000/db/getUser/${auth.sessionID}`, {
       method: "PUT",
       headers: {
@@ -23,25 +38,55 @@ const Profile = ({auth}) => {
       },      
       body: JSON.stringify({
         description: description,
-        age: age,
+        birthday: birthday,
         gender: gender
       })
     })
+    setEdit(false)
+    console.log("AAAA", birthday)
+  }
+
+  const handleCancel = () => {
+    let {description, birthday, gender, } = prevRef.current
+    setDescription(description)
+    setBirthday(birthday)
+    setGender(gender)
+    setEdit(false)
   }
 
   if(edit) return(
-    <div>
-      <textarea type="textarea" value={description} onChange={e => setDescription(e.target.value)} />
-      <p> Age: {age} </p>
-      <p> Gender: {gender} </p>
-      <button onClick={save} > Save </button>
+    <div className="edit-profile">
+      <label>
+        <span>About Me: </span>
+        <textarea id="Description" type="textarea" value={description} onChange={e => setDescription(e.target.value)} />
+      </label>
+      <BirthdayInput 
+        minimumAge={13} 
+        callback={setBirthday}
+        initial={birthday}
+        />
+      <label>
+      <span> Gender: </span>
+      <Select
+        placeholder = "Select your gender"
+        onChange={selected => setGender(selected.value)}
+        options = {[
+          {value: "Male", label: "Male"},
+          {value: "Female", label: "Female"},
+          {value: "other", label: "Other"}
+        ]}
+      />
+      </label>
+
+      <button onClick={handleSave} > Save </button>
+      <button onClick={handleCancel} > Cancel </button>
     </div>
   )
   return(
     <div>
-      <button onClick={() => setEdit(true)}>Edit</button>
+      <button onClick={handleEdit}>Edit</button>
       <p> Description: {description} </p>
-      <p> Age: {age} </p>
+      <p> Birthday: {moment(birthday).format("YYYY-MM-DD")} </p>
       <p> Gender: {gender} </p>
     </div>
   )
