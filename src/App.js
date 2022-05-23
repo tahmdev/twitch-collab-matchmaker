05f25110217a34c726388af9abd2f594659639d2
tahmdev <qwertiimusic@gmail.com>
2022-05-23 18:11:13 +0200

    Added socket.io

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Cookies from 'js-cookie';
-import { useState } from 'react';
+import io from "socket.io-client";
+
+import { useEffect, useState } from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import Navbar from './components/navbar';
 import Profile from './routes/profile';
 import Ideal from './routes/ideal';
 import Match from './routes/match';
-
-// Clear matches on ideal change
+import Chat from './routes/chat';
+const ENDPOINT = "http://localhost:9000/";
+const socket = io(ENDPOINT)
 // Match
-// => On decline
-//  => add to declined list 
 // => On accept 
 //  => add to accepted list
 //    => Check if partners accepted list includes user 
@@ -27,6 +28,7 @@ import Match from './routes/match';
 // Generate new sessionID until found one that is unique
 // Also allow multiple sessionIDs
 // Create ideal entry on first login
+// birthday-input: Only show error message if not focused 
 
 function App() {
   let [auth, setAuth] = useState(() => {
@@ -41,6 +43,7 @@ function App() {
         <Route path="/profile" element={ <Profile auth={auth} /> } />
         <Route path="/ideal" element={ <Ideal auth={auth} /> } />
         <Route path="/match" element={ <Match auth={auth} /> } />
+        <Route path="/chat" element={ <Chat auth={auth} socket={socket} /> } />
       </Routes>
     </div>
   );
