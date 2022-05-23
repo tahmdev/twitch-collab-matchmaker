b7f9c960249ba49df936d1007d4574302cc87859
tahmdev <qwertiimusic@gmail.com>
2022-05-23 18:14:45 +0200

    Added get chat patners

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -2,5 +2,23 @@ import { useEffect, useRef, useState } from "react"
 import ChatWindow from "../components/chatWindow"
 
 const Chat = ({socket, auth}) => {
+  let [chatPartners, setChatPartners] = useState()
+  useEffect(() => {
+    fetch(`http://localhost:9000/chat/getChats/${auth.sessionID}`)
+    .then(res => res.json())
+    .then(json => setChatPartners(json))
+
+    socket.on("newMessage", msg => handleNewMessage(msg))
+  }, [])
+      { 
+        chatPartners &&
+        chatPartners.map(partner => {
+          return(
+            <button key={partner.id} onClick={() => openChat(partner)}>
+              {partner.name}
+            </button>
+          )
+        })
+      }
 }
 export default Chat
