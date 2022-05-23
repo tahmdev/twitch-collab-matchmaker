5896299306fed4e17d3061c69c5f43db9cf94920
tahmdev <qwertiimusic@gmail.com>
2022-05-23 18:15:41 +0200

    Added open chat functionality

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -2,7 +2,10 @@ import { useEffect, useRef, useState } from "react"
 import ChatWindow from "../components/chatWindow"
 
 const Chat = ({socket, auth}) => {
+
   let [chatPartners, setChatPartners] = useState()
+  let [messages, setMessages] = useState()
+  let currentChatRef = useRef()
   useEffect(() => {
     fetch(`http://localhost:9000/chat/getChats/${auth.sessionID}`)
     .then(res => res.json())
@@ -10,6 +13,12 @@ const Chat = ({socket, auth}) => {
 
     socket.on("newMessage", msg => handleNewMessage(msg))
   }, [])
+  const openChat = (partner) => {
+    fetch(`http://localhost:9000/chat/getHistory/${auth.sessionID}/${partner.id}`)
+    .then(res => res.json())
+    .then(json => setMessages(json))
+    currentChatRef.current = partner
+  }
       { 
         chatPartners &&
         chatPartners.map(partner => {
