9974ea59456a71fd1a81cbbc66ad99a958b0258e
tahmdev <qwertiimusic@gmail.com>
2022-05-24 19:58:59 +0200

    Made textarea resize to fit content

--- src/components/chatWindow.js
+++ src/components/chatWindow.js
@@ -1,8 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ChatMessage from "./chatMessage"
 
 const ChatWindow = ({messages, sendMessage, partner, handleBack}) => {
   let [currentInput, setCurrentInput] = useState("")
+
+  const resizeTextarea = () => {
+    let el = document.getElementById("chat-textarea")
+    el.style.height = `Calc(1.2rem)`
+    el.style.height = `Calc(${el.scrollHeight}px - 1rem)`
+  }
+
+  useEffect(() => {
+    let el = document.getElementById("chat-textarea")
+    el.addEventListener("input", resizeTextarea)
+    return(() => el.removeEventListener("input", resizeTextarea))
+  }, [resizeTextarea])
 
   const handleSend = () => {
     sendMessage(currentInput)
