46df7bdb696874d08d0b9d9966cfc91f225a27e1
tahmdev <qwertiimusic@gmail.com>
2022-05-26 18:03:18 +0200

    Replaced Number inputs with Select inputs

--- src/components/birthdayInput.js
+++ src/components/birthdayInput.js
@@ -3,7 +3,10 @@ import moment from "moment"
 import { useEffect, useState } from "react"
 
 const BirthdayInput = ({minimumAge, callback, initial}) => {
-  let [day, setDay] = useState(moment(initial).format("DD"))
+  let [day, setDay] = useState(() => {
+    let DD = moment(initial).format("DD")
+    return {value: DD, label: DD}
+  })
   let [month, setMonth] = useState(() => {
     switch(moment(initial).format("MM")){
       case "01":
@@ -32,7 +35,10 @@ const BirthdayInput = ({minimumAge, callback, initial}) => {
         return {value: 12, label: "December"}
     }
   })
-  let [year, setYear] = useState(moment(initial).format("YYYY"))
+  let [year, setYear] = useState(() => {
+    let YYYY = moment(initial).format("YYYY")
+    return {value: YYYY, label: YYYY}
+  })
   let [validDate, setValidDate] = useState(false)
 
   const padZeros = (number, length, pos) => {
@@ -45,51 +51,81 @@ const BirthdayInput = ({minimumAge, callback, initial}) => {
     return result
   }
 
+  const getMinimumBirthday = (minAge = 0) => {
+    return moment().subtract(minAge, "years")
+  }
+
   useEffect(() => {
-    let minimumBirthday = moment().subtract(minimumAge || 0, "years").format("YYYY-MM-DD");
-    let date = moment(`${year}-${padZeros(month.value, 2)}-${padZeros(day, 2)}`, "YYYY-MM-DD", true)
+    let minimumBirthday = getMinimumBirthday(minimumAge).format("YYYY-MM-DD");
+    let date = moment(`${year.value}-${padZeros(month.value, 2)}-${padZeros(day.value, 2)}`, "YYYY-MM-DD", true)
     
     let dateIsValid = date.isBefore(minimumBirthday) && date.isValid()
     setValidDate(dateIsValid)
     callback(date.format("YYYY-MM-DD HH:mm:ss"), dateIsValid)
   }, [day, month, year])
 
+  const getRange = (end, start) => {
+    let result = []
+    for(let i = start || 0; i <= end; i++){
+      result.push(i)
+    }
+    return(result)
+  }
+
   const maxYear = moment().subtract(minimumAge, "years").format("YYYY")
   return(
     <div>
-      <div className="flex-row" >
-        <label>
-        <span>Month:</span>
-        <Select
-          id = "month"
-          value={month}
-          onChange={setMonth}
-          placeholder="Month"
-          options={
-            [
-              {value: 1, label: "January"},
-              {value: 2, label: "February"},
-              {value: 3, label: "March"},
-              {value: 4, label: "April"},
-              {value: 5, label: "May"},
-              {value: 6, label: "June"},
-              {value: 7, label: "July"},
-              {value: 8, label: "August"},
-              {value: 9, label: "September"},
-              {value: 10, label: "October"},
-              {value: 11, label: "November"},
-              {value: 12, label: "December"},
-            ]
-          } 
+      <div className="flex-row birthday-input" >
+      <label className='days' >
+        <span>Day:</span>
+        <Select 
+        classNamePrefix='birthday-select'
+        value={day}
+        onChange={setDay}
+        placeholder="Day"
+        options={getRange(31, 1).map(i => {return {value: i, label: i}})} 
         />
+      </label>
+        <label className='months' >
+        <span>Month:</span>
+          <Select
+            classNamePrefix='birthday-select'
+            id = "month"
+            value={month}
+            onChange={setMonth}
+            placeholder="Month"
+            options={
+              [
+                {value: 1, label: "January"},
+                {value: 2, label: "February"},
+                {value: 3, label: "March"},
+                {value: 4, label: "April"},
+                {value: 5, label: "May"},
+                {value: 6, label: "June"},
+                {value: 7, label: "July"},
+                {value: 8, label: "August"},
+                {value: 9, label: "September"},
+                {value: 10, label: "October"},
+                {value: 11, label: "November"},
+                {value: 12, label: "December"},
+              ]
+            } 
+          />
         </label>
-        <label>
-          <span>Day:</span>
-          <input id="day" type="number" min={1} max={31} value={day} onChange={e => setDay(e.target.value)} />
-        </label>
+        
         <label>
           <span>Year:</span>
-          <input id="year" type="number" min={1900} max={maxYear} value={year} onChange={e => setYear(e.target.value)} />
+          {/* <input id="year" type="number" min={1900} max={maxYear} value={year} onChange={e => setYear(e.target.value)} /> */}
+          <Select 
+          classNamePrefix='birthday-select'
+          value={year}
+          onChange={setYear}
+          placeholder="Year"
+          options={getRange(getMinimumBirthday(minimumAge).format("YYYY"), 1900)
+            .sort((a, b) => b - a)
+            .map(i => {return {value: i, label: i}})
+          }
+          />
         </label>
       </div>
       <p style={{display: validDate ? "none" : "block"}} > Please enter a valid date </p>
