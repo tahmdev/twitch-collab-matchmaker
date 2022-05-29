8ebd8b51b1d43fc7253158ae53eb3b332baf1b3c
tahmdev <qwertiimusic@gmail.com>
2022-05-29 21:27:01 +0200

    Implemented and styled popup button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Cookies from 'js-cookie';
+import { Link, useNavigate } from 'react-router-dom';
+import PopupButton from './popup-button';
+import { faUser, faArrowRightFromBracket, faGear, faHeart  } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
 
-const Navbar = ({auth}) => {
+const Navbar = ({auth, setAuth}) => {
+  const [showUserPopup, setShowUserPopup] = useState(false)
+  const navigate = useNavigate();
+  const logout = () => {
+    Cookies.remove("twitchMatchMakerauthCookie")
+    setAuth(null)
+    navigate("/")
+  }
+
+  const popup = [
+    {
+      name: "Profile",
+      icon: faUser,
+      role: "link",
+      onClick: () => {navigate("/profile"); setShowUserPopup(false)},
+    },
+    {
+      name: "Ideal",
+      icon: faHeart,
+      role: "link",
+      onClick: () => {navigate("/ideal"); setShowUserPopup(false)},
+    },
+    {
+      name: "Settings",
+      icon: faGear,
+      role: "button",
+      onClick: () => {console.log("aa"); setShowUserPopup(false)},
+    },
+    {
+      name: "Logout",
+      icon: faArrowRightFromBracket,
+      role: "button",
+      onClick: () => logout(),
+    },
+  ]
 
   return(
     <nav>
@@ -16,10 +55,31 @@ const Navbar = ({auth}) => {
       
       {auth &&
         <>
-          <button onClick={() => Cookies.remove("twitchMatchMakerauthCookie")}> Logout </button>
-          <p> {auth.name} </p>
-          <p> {auth.sessionID} </p>
-          <img src={auth.profilePicture} />
+          <Link to="/match" >Match me</Link>
+          <PopupButton
+            classes={"trans-btn user-btn"}
+            popupClasses={"user-popup"}
+            show={showUserPopup}
+            setShow={setShowUserPopup}
+          >
+            <div className='nav-user flex-row'>
+              <p> {"auth.name"} </p>
+              <img src={auth.profilePicture} />
+            </div>
+
+            <ul>
+              {popup.map(i => {
+                return(
+                  <li key={i.name} >
+                    <button onClick={i.onClick} role={i.role} >
+                      <FontAwesomeIcon icon={i.icon} className="user-icon" />
+                      <span>{i.name}</span>
+                    </button>
+                  </li>
+                )
+              })}
+            </ul>
+          </PopupButton>
         </>
       }
     </nav>
