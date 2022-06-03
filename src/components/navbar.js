c852d906a6b182a31df836392578fd3edef0293c
tahmdev <qwertiimusic@gmail.com>
2022-06-03 19:09:10 +0200

    Added lightmode switch

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Cookies from 'js-cookie';
+import useLocalstorage from '../hooks/useLocalStorage';
 import { Link, useNavigate } from 'react-router-dom';
 import PopupButton from './popup-button';
 import { faUser, faArrowRightFromBracket, faGear, faHeart, faMessage  } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Popup from './popup';
+import Switch from './switch';
 
 const Navbar = ({auth, setAuth}) => {
   const [showUserPopup, setShowUserPopup] = useState(false)
@@ -50,6 +52,31 @@ const Navbar = ({auth, setAuth}) => {
     },
   ]
 
+  useEffect(() => {
+    let root = document.documentElement;
+    if(lightmode){
+      root.style.setProperty("--bg-100", "white")
+      root.style.setProperty("--bg-200", "rgb(252, 252, 252)")
+      root.style.setProperty("--bg-250", "rgb(233, 233, 233)")
+      root.style.setProperty("--bg-300", "rgb(218, 218, 218)")
+      root.style.setProperty("--bg-400", "rgb(165, 165, 165)")
+      root.style.setProperty("--primary-color-300", "rgb(117,46,228)")
+      root.style.setProperty("--primary-color-600", "rgb(89, 131, 236)")
+      root.style.setProperty("--text-color", "black")
+      root.style.setProperty("--border-color", "rgba(0, 0, 0, 0.493)")
+    }else{
+      root.style.setProperty("--bg-100", "rgb(43, 43, 43)")
+      root.style.setProperty("--bg-200", "rgb(70, 70, 70)")
+      root.style.setProperty("--bg-250", "rgb(70, 70, 70)")
+      root.style.setProperty("--bg-300", "rgb(122, 122, 122)")
+      root.style.setProperty("--bg-400", "rgb(122, 122, 122)")
+      root.style.setProperty("--primary-color-300", "rgb(144, 89, 231)")
+      root.style.setProperty("--primary-color-600", "rgb(89, 248, 195)")
+      root.style.setProperty("--text-color", "white")
+      root.style.setProperty("--border-color", "rgba(255, 255, 255, 0.493)")
+    }
+  }, [lightmode])
+
   return(
     <nav>
     {showSettingsPopup &&
@@ -59,6 +86,17 @@ const Navbar = ({auth, setAuth}) => {
       >
         <div className='settings'>
           <h2>Settings</h2>
+          <ul>
+            <li className='flex-row'>
+              <span> Theme: </span>
+              <Switch 
+                id="lightmode" 
+                checked={lightmode} 
+                onChange={e => setLightmode(e.target.checked)} 
+                classes="lightmode-switch"
+              />
+            </li>
+          </ul>
         </div>
       </Popup>
     }
@@ -82,7 +120,7 @@ const Navbar = ({auth, setAuth}) => {
                   <li key={i.name} >
                     <button onClick={i.onClick} role={i.role} >
                       <FontAwesomeIcon icon={i.icon} className="user-icon" />
-                      <span>{i.name}</span>
+                      <span className='fix-text'>{i.name}</span>
                     </button>
                   </li>
                 )
